aaa3a0dd934533b923420d62f74d8856d0aacfa8
Long Tran <LongTran>
2022-12-29 20:58:56 +0700

    refactored directory names

---
+++ EStoreWebParent/EStoreB2C/src/main/java/com/shopme/category/CategoryService.java
@@ -0,0 +1,31 @@
+package com.shopme.category;
+
+import com.shopme.common.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repo;
+
+    public List<Category> listNoChildrenCategories() {
+        List<Category> listNoChildrenCategories = new ArrayList<>();
+
+        List<Category> listEnabledCategories = repo.findAllEnabled();
+
+        listEnabledCategories.forEach(category -> {
+            Set<Category> children = category.getChildren();
+            if (children == null || children.size() == 0) {
+                listNoChildrenCategories.add(category);
+            }
+        });
+
+        return listNoChildrenCategories;
+    }
+}
---
+++ EStoreWebParent/EStoreB2C/src/main/java/com/shopme/security/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.shopme.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests().anyRequest().permitAll();
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/images/**",
+                "/js/**",
+                "/webjars/**",
+                "/styles/**",
+                "/error");
+    }
+}
